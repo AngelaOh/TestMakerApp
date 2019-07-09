a9a44bde185308be580e197ad719c5cb75309c3d
Angela <angela.ohh@gmail.com>
2019-07-09 12:53:38 -0700

    added Volley Library for API calls

--- app/src/main/java/com/example/testmakerapp/DashboardActivity.java
+++ app/src/main/java/com/example/testmakerapp/DashboardActivity.java
@@ -2,13 +2,45 @@ package com.example.testmakerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
+    private TextView outputText;
+    private TextView headerTwo;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+//        outputText = findViewById(R.id.practice_output);
+        headerTwo = findViewById(R.id.app_header2);
+        backButton = findViewById(R.id.back_button);
+
+        String practiceValue = String.valueOf(getIntent().getStringExtra("practice_input"));
+
+        if (practiceValue != null ) {
+//            outputText.setText(practiceValue);
+            headerTwo.setText("Welcome, " + practiceValue);
+        }
+
+        Log.d("Intent", "onCreate: what is being passed in" + practiceValue);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                intent.putExtra("dashboard_callback", "Message from dashboard");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/testmakerapp/MainActivity.java
+++ app/src/main/java/com/example/testmakerapp/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.testmakerapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button practiceButton;
+    private EditText practiceInput;
+    private final int REQUEST_CODE = 123;
+    private String practiceURL = "https://jsonplaceholder.typicode.com/todos/1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +24,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         practiceButton = findViewById(R.id.practice_button);
+        practiceInput = findViewById(R.id.practice_input);
+
+        Log.d("MainActivity Input", "onCreate: practiceInput valid?" + practiceInput.getText());
 
         practiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String input = practiceInput.getText().toString().trim();
                 Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
-                startActivity(intent);
+                intent.putExtra("practice_input", input);
+                startActivityForResult(intent, REQUEST_CODE);
+                // startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            String message = data.getStringExtra("dashboard_callback");
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
     }
 }
