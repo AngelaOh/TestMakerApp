3e9f7bda0ffd63c505071e63e60403700502407e
Angela <angela.ohh@gmail.com>
2019-07-09 09:47:59 -0700

    added functionality to switch views

--- app/src/main/java/com/example/testmakerapp/MainActivity.java
+++ app/src/main/java/com/example/testmakerapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.testmakerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button practiceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        practiceButton = findViewById(R.id.practice_button);
+
+        practiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
